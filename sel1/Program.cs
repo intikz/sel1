670af9c9535203f3d88905d920fdeabf834c208c
intikz <inti.acostaylara@kzsoftworks.com>
2021-05-21 10:57:16 -0300

    Add project files.

---
+++ sel1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sel1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
